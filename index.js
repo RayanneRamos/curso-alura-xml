5104495ebabb971eeee865ab8f73d159b5dc5309
Rayanne Ramos <43352880+RayanneRamos@users.noreply.github.com>
2021-02-22 16:06:14 -0300

    Convertendo para formato xml

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.xml());
+
+app.use((req, res, prox) => {
+  let formatoRequisitado = req.header("Accept");
+
+  if(formatoRequisitado === "*/*") {
+    formatoRequisitado = "application/json";
+  }
+
+  if(formatosAceitos.indexOf(formatoRequisitado) === -1) {
+    res.status(406);
+    res.end();
+    return;
+  }
+
+  res.setHeader("Content-Type", formatoRequisitado);
+  prox();
+});
 
 app.listen(3000, () => console.log("Server iniciado!"));
